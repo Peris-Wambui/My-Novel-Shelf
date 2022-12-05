5c6de4d74e5ee4b367727ea9f4251f8d28318191
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-12-05 11:54:48 -0500

    seed some novel data

--- app/controllers/novels_controller.rb
+++ app/controllers/novels_controller.rb
@@ -1,4 +1,7 @@
 class NovelsController < ApplicationController
     # all the crud functionalities
     # index, create, show update destroy
+    def index
+        
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+class Seed
+    def self.begin
+        seed =Seed.new
+        seed.generate_novels
+    end
+    def generate_novels
+        5.times do |i|
+            novel = Novel.create!(
+                title:Faker::Movie.title,
+                author:Faker::Name.name,
+                description:Faker::Lorem.paragraph(sentence_count:4)
+            )
+        end
+    end
+end
+
+Seed.begin
