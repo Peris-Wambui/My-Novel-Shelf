9bd9bfa187ffbc46f765166ec3771e172d76cfb8
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-12-05 12:11:22 -0500

    find all novels by index method

--- app/controllers/novels_controller.rb
+++ app/controllers/novels_controller.rb
@@ -2,6 +2,16 @@ class NovelsController < ApplicationController
     # all the crud functionalities
     # index, create, show update destroy
     def index
-        
+        @novels = Novel.all
+        # render json: @novels, status:200
+        json_res(@novels)
+    end
+
+    
+
+    private
+    def json_res(object,status = :ok)
+        render json: object, status: status
     end
 end
+
