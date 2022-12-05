790923cf7fdf03f5039be1adb94a26fecc6fd2cc
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-12-05 15:07:20 -0500

    install bcryt

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :novels, ath: 'api/novels'
+  resources :novels, path: 'api/novels'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
