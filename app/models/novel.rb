96a8ce81c6200c938334842c3d1c39e94cede308
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-12-05 15:27:38 -0500

    add relationship

--- app/models/novel.rb
+++ app/models/novel.rb
@@ -2,6 +2,6 @@ class Novel < ApplicationRecord
     validates :title, presence: true
     validates :author, presence: true
     validates :description, presence: true
-    
-    belongs_to
+
+    belongs_to :bookworm
 end
