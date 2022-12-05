dda7c9b652767f9ba06611cb1bfe48d53509284c
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-12-05 15:38:30 -0500

    new migration to connenct novel and bookworm

--- db/migrate/20221205201221_create_bookworms.rb
+++ db/migrate/20221205201221_create_bookworms.rb
@@ -1,6 +1,7 @@
 class CreateBookworms < ActiveRecord::Migration[7.0]
   def change
     create_table :bookworms do |t|
+      
       t.column :email, :string
       t.column :password_hash, :string
       t.column :password_salt, string
