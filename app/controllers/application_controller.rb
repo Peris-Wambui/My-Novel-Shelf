3bcf822a4631886b0a38e51deade35c01ec6be50
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-12-05 15:03:56 -0500

    progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::API
     include ActionController::Cookies
     include Response
 
+rescue_from ActiveRecord::RecordInvalid do |exception|
+    json_res({message: exception.message}, status = 422)
+end
+
+rescue_from ActiveRecord::RecordNotFound do |exception|
+    json_res({message: exception.message}, status = 404)
+end
+
     def hello_world
         session[:count] = (session[:count] || 0) + 1
         render json: { count: session[:count] }
