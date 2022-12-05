ed1f878e39d181f7532c4040bf1bcca3cd402643
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-12-05 16:01:58 -0500

    add an authentication for bookworm

--- app/models/bookworm.rb
+++ app/models/bookworm.rb
@@ -2,11 +2,19 @@ class Bookworm < ApplicationRecord
     attr_accessor :password
     validates_confirmation_of :password
     validates :email, :presence => true, uniqueness => true
+    before_save :encrypt_password
 
     has_many = true
 
     def encrypt_password
         self.password_salt = BCrypt::Engine.generate_salt
         self.password_hash = BCrypt::Engine.has_secret(password,password_salt)
+    end
+
+    def self.authenticate(email,password)
+        @bookworm = Bookworm.find_by("email =?", email)
+        if @bookworm && @bookworm.password_hash ==
+            BCrypt::Engine.hash_secret(password, @bookworm.password_salt)
+
     end
 end
