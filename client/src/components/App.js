532993f2a7f51c6e61365fc1357f34cdb3b90acb
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-12-09 12:45:09 +0300

    added components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,40 @@
-import { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { Switch, Route } from "react-router-dom";
+import NavBar from "./NavBar";
+import Login from "../pages/Login";
+import RecipeList from "../pages/RecipeList";
+import NewRecipe from "../pages/NewRecipe";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
   }, []);
 
+  if (!user) return <Login onLogin={setUser} />;
+
   return (
-    <div className="App">
-      <h1>Page Count: {count}</h1>
-    </div>
+    <>
+      <NavBar user={user} setUser={setUser} />
+      <main>
+        <Switch>
+          <Route path="/new">
+            <NewRecipe user={user} />
+          </Route>
+          <Route path="/">
+            <RecipeList />
+          </Route>
+        </Switch>
+      </main>
+    </>
   );
 }
 
 export default App;
+
